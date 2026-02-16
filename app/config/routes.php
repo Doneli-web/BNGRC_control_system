716a3c67205ea820938455dee4b93dfec965174c
brandon0638 <brandonandria06@gmail.com>
2026-02-16 17:39:59 +0300

    modification de route et mis a jour

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,7 +2,9 @@
 
 use app\controllers\VilleController;
 use app\controllers\RegionController;
+use app\controllers\DonController;
 use app\controllers\TypeDonController;
+use app\controllers\ArticleController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -36,16 +38,49 @@ $router->group('/', function(Router $router) use ($app) {
 
     $router->get('/besoins', function() use($app){
         $typeDon = TypeDonController::findAll();
+        $articles = ArticleController::findAll();
         $app->render('besoins', [
-            "typeDon" => $typeDon
+            "typeDon" => $typeDon,
+            "articles" => $articles
         ]);
+    });
+
+    $router->get('/villes/stats', function() {
+        $data = VilleController::getVilleStats();
+        Flight::json($data);
+    });
+
+    // API pour dashboard complet
+    $router->get('/dashboard', function() {
+        $data = VilleController::getDashboard();
+        Flight::json($data);
+    });
+
+
+    $router->get('/api/article/@id', function($id) use ($app){
+        $article = ArticleController::findById($id);
+        echo json_encode(["prix_unitaire"=>$article["prix_unitaire"]]);
+    });
+
+       $router->get('/dons', function() use($app){
+        DonController::showDonsPage($app);
+    });
+
+   
+
+    $router->post('/dons/add', function() use($app){
+        DonController::addDonFromForm();
+    });
+
+    $router->get('/dons/delete/@id', function($id) use($app){
+        DonController::deleteDon($id);
+        $_SESSION['success'] = "Don supprimé avec succès";
+        Flight::redirect('/dons');
     });
 
     $router->get('/*', function() use($app){
         $app->render('404', []);
     });
-
-
 
    
     // $router->post('/register', function() use($app){
