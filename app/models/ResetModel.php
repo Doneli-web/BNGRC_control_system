157028b431d6d58caa15a8b729481052e7f37c50
Doneli-web <solofodoneli37@gmail.com>
2026-02-17 12:12:23 +0100

    update reset

--- app/models/ResetModel.php
+++ app/models/ResetModel.php
@@ -16,6 +16,8 @@ class ResetModel {
         try {
             $this->db->beginTransaction();
           
+            $this->db->exec("UPDATE BNGRC_don SET status = 'non_commence'");
+            $this->db->exec("UPDATE BNGRC_besoin SET status = 'non_utilise'");
             $this->db->exec("DELETE FROM BNGRC_dispatch");
             $this->db->exec("DELETE FROM BNGRC_achat");
             
